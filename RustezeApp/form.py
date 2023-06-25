8f4b04e75b46e89af33da210fc51722c9f409f14
Sebastián G <seba.gallardog@duocuc.cl>
2023-06-25 18:17:58 -0400

    Formulario registro, inicio de sesion

--- RustezeApp/form.py
+++ RustezeApp/form.py
@@ -1,31 +1,63 @@
-from django import forms
-from django.forms import fields
 from .models import Producto
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 
 
 class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'precio']
+
+
+class Registro(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs = {'class': 'form-control'}
+        self.fields['password2'].widget.attrs = {'class': 'form-control'}
 
     class Meta:
-       model = Producto
-       fields = [ 'nombre', 'descripcion','precio']
-    #    widgets = {
-    #         'nombre': forms.TextInput(
-    #             attrs = {
-    #                 'class':'form-control',
-    #                 'placeholder':'Nombre del Producto'
-    #             }
-    #         ),
-    #         'descripcion': forms.TextInput(
-    #             attrs = {
-    #                 'class':'form-control',
-    #                 'placeholder':'Descripcion'
-    #             }
-    #         ),
-    #         'precio': forms.NumberInput(
-    #             attrs = {
-    #                 'class':'form-control',
-    #                 'placeholder':'Precio'
-    #             }
-    #         )
-    #         }
+        model = User
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'first_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+        }
+
+
+class IniciarSesion(forms.Form):
+    username = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Nombre de Usuario'
+        }
+    ))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Contraseña'
+        }
+    ))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
