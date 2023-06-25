483e30b90024971fb0ba5c9b62e7899581154757
Sebastián G <seba.gallardog@duocuc.cl>
2023-06-25 18:16:00 -0400

    Url login registro

--- RustezeApp/urls.py
+++ RustezeApp/urls.py
@@ -3,14 +3,17 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    
-    path('',views.Index,  name='Index'),
-    #path('',views.Base,  name='Base'),
-    path('Tienda',views.Tienda,  name='Tienda'),
-    path('SobreNosotros',views.SobreNosotros,  name='SobreNosotros'),
-    path('Contacto',views.Contacto,  name='Contacto'),
-    path('IniciarSesion',views.IniciarSesion,  name='IniciarSesion'),
-    path('Registro',views.Resgistro,  name='Registro'),
-    path('ListarProductos',views.listar_productos,  name='ListarProductos'),
-    path('Agregar', views.agregar_producto , name="AgregarProducto" ),
+
+    path('', views.index, name='Index'),
+    path('Tienda', views.tienda, name='Tienda'),
+    path('SobreNosotros', views.sobre_nosotros, name='SobreNosotros'),
+    path('Contacto', views.contacto, name='Contacto'),
+    path('IniciarSesion', views.LogIn, name='IniciarSesion'),
+    path('Registro', views.resgistro, name='Registro'),
+    path('ListarProductos', views.listar_productos, name='ListarProductos'),
+    path('Agregar', views.agregar_producto, name='AgregarProducto'),
+    #path('login', views.show_login, name='login'),
+    path('register', views.mostrar_registro, name='register'),
+    path('cerrar', views.cerrar_sesion, name='CerrarSesion'),
+    path('mostrar_iniciar_sesion/', views.mostrar_iniciar_sesion, name='mostrar_iniciar_sesion'),
 ]
