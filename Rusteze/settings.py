1b312d9c523f3e5dabd8f7f26a8f9ad58e2c970d
Sebastián G <103399293+sbgallardo@users.noreply.github.com>
2023-06-29 15:59:58 -0400

    Update settings.py

--- Rusteze/settings.py
+++ Rusteze/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6mqv&e0xbf(-i4u-72r7vrzhy0g2eavpuf&!^l+v4+*ucsqynh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -94,7 +94,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'RustezeApp/static')
 
