967a626f84ecd25f19ed84ed28ea9554166fbab7
Sebastián G <seba.gallardog@duocuc.cl>
2023-07-04 23:56:00 -0400

    Correccion de errores

--- Rusteze/settings.py
+++ Rusteze/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6mqv&e0xbf(-i4u-72r7vrzhy0g2eavpuf&!^l+v4+*ucsqynh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -93,6 +93,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+LOGIN_URL = '/IniciarSesion'
 
 STATIC_URL = '/static/'
 
